da162a9c9b8aa9d494583d784e9e773fa85f0ada
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-04-07 16:39:53 +0300

    add chat/info

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from typing import Optional
+
 from fastapi import FastAPI
+
+from analyzer.tool import load_json, chat_info
 
 app = FastAPI()
 
@@ -8,6 +12,14 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+@app.get("/chat/info")
+async def get_chat_info(file_path: Optional[str] = 'result.json'):
+    # Load JSON data
+    data = load_json(file_path)
+    if data is None:
+        return {"message": "Failed to load JSON data"}
+
+    # Extract chat information
+    chat_information = chat_info(data)
+
+    return chat_information
