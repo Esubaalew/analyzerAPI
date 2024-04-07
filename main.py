249eba7c5180bc2984671463ffcdbeaed7707d69
Esubalew Chekol <esubalew.chekol-ug@aau.edu.et>
2024-04-07 18:18:21 +0300

    add /average-message-length

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from typing import Optional
 from fastapi import FastAPI
 
 from analyzer.tool import load_json, chat_info, get_oldest_message, get_latest_message, get_senders, \
-    get_forwarded_messages, get_forwarders, get_forward_sources, get_repliers, get_editors, get_most_common_words
+    get_forwarded_messages, get_forwarders, get_forward_sources, get_repliers, get_editors, get_most_common_words, \
+    each_average_message_length
 
 app = FastAPI()
 
@@ -110,3 +111,14 @@ async def get_most_common_words_endpoint(file_path: Optional[str] = 'result.json
     most_common_words = get_most_common_words(data, top_n)
 
     return most_common_words
+
+
+@app.get("/average-message-length")
+async def calculate_average_message_length(file_path: Optional[str] = 'result.json'):
+    data = load_json(file_path)
+    if data is None:
+        return {"message": "Failed to load JSON data"}
+
+    average_lengths = each_average_message_length(data)
+
+    return average_lengths
